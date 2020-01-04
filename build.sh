7d99c3d4bf7f08e6effcf84903e7c655e853b96b
Leon York <leon@leonyork.com>
2020-01-04 22:08:45 +0000

    Exclude Ansible release candidates

--- build.sh
+++ build.sh
@@ -6,24 +6,27 @@
 #######################################################################
 set -eux
 
+# Number of releases to go back from the latest version
 number_of_ansible_releases=5
 number_of_alpine_releases=5
 
 # Creates tags of the form {ANSIBLE_VERSION}-alpine-{ALPINE_VERSION} (e.g. 2.9.2-alpine-3.11.0)
 # First gets the last $number_of_alpine_releases alpine tags where the tag looks like a version number (there were some odd tags that look like dates)
-# For each of those tags gets the last $number_of_ansible_releases and builds an image
+# For each of those tags gets the last $number_of_ansible_releases of non-release candidate versions of Ansible and builds an image
 docker run leonyork/docker-tags library/alpine \
     | grep -E '^[0-9.]\.[0-9.]+$' \
     | tail -n $number_of_alpine_releases \
     | xargs -I{ALPINE_VERSION} -n1 \
         sh -c "docker run leonyork/pypi-releases ansible \
+        | grep -E '^[0-9.]\.[0-9.]+$' \
         | tail -n $number_of_ansible_releases \
         | xargs -I{ANSIBLE_VERSION} -n1 sh build-image.sh {ALPINE_VERSION} {ANSIBLE_VERSION} {ANSIBLE_VERSION}-alpine-{ALPINE_VERSION} || exit 255" || exit 255
 
 # Creates tags of the form {ANSIBLE_VERSION}-alpine (e.g. 2.9.2-alpine)
-# Gets the last $number_of_ansible_releases and creates an image using the latest alpine image
+# Gets the last $number_of_ansible_releases of non-release candidate versions of Ansible and creates an image using the latest alpine image
 docker run leonyork/pypi-releases ansible \
         | tail -n $number_of_ansible_releases \
+        | grep -E '^[0-9.]\.[0-9.]+$' \
         | xargs -I{ANSIBLE_VERSION} -n1 sh build-image.sh latest {ANSIBLE_VERSION} {ANSIBLE_VERSION}-alpine || exit 255
 
 # Generates the latest tag
