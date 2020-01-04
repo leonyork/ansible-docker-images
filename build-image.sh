80c505b250f163fd825366b88779da6da09d39a3
Leon York <leon@leonyork.com>
2020-01-04 22:08:22 +0000

    Cut down logging

--- build-image.sh
+++ build-image.sh
@@ -5,7 +5,6 @@
 # Second argument is the ansible version to use
 # Third argument is the image tag
 #######################################################################
-set -eux
 
 if [ -z "$1" ]
 then
@@ -25,13 +24,18 @@ then
     exit 1
 fi
 
+set -eux
 export ALPINE_VERSION=$1;
 export ANSIBLE_VERSION=$2;
 export IMAGE_TAG=$3;
-docker-compose build 
+# Don't log this as there is so too much logging
+docker-compose build > /dev/null
 
 # Test that it's working by making sure we can get the version
 docker-compose run ansible --version
 
 # Push to the docker registry
 docker-compose push
+
+# Remove any images left around
+docker-compose down --rmi all 2> /dev/null
