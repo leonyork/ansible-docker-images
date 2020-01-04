e7db08e96b975ab16d64dbeacde5da0350cc7efc
Leon York <leon@leonyork.com>
2020-01-04 21:08:54 +0000

    Add build of multiple version

---
+++ build-image.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env sh
+#######################################################################
+# Build, test and push an image
+# First argument is the alpine version to use
+# Second argument is the ansible version to use
+# Third argument is the image tag
+#######################################################################
+set -eux
+
+if [ -z "$1" ]
+then
+    echo "You must specify the version of the docker image alpine to use"
+    exit 1
+fi
+
+if [ -z "$2" ]
+then
+    echo "You must specify the tersion of ansible to use"
+    exit 1
+fi
+
+if [ -z "$3" ]
+then
+    echo "You must specify the image tag to use"
+    exit 1
+fi
+
+export ALPINE_VERSION=$1;
+export ANSIBLE_VERSION=$2;
+export IMAGE_TAG=$3;
+docker-compose build 
+
+# Test that it's working by making sure we can get the version
+docker-compose run ansible --version
+
+# Push to the docker registry
+#docker-compose push
---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+#######################################################################
+# Build, test and push the images
+# Creates multiple versions so that there's some choice about versions
+# to use
+#######################################################################
+set -eux
+
+number_of_ansible_releases=1
+number_of_alpine_releases=1
+
+# Creates tags of the form {ANSIBLE_VERSION}-alpine-{ALPINE_VERSION} (e.g. 2.9.2-alpine-3.11.0)
+# First gets the last $number_of_alpine_releases alpine tags where the tag looks like a version number (there were some odd tags that look like dates)
+# For each of those tags gets the last $number_of_ansible_releases and builds an image
+docker run leonyork/docker-tags library/alpine \
+    | grep -E '^[0-9.]\.[0-9.]+$' \
+    | tail -n $number_of_alpine_releases \
+    | xargs -I{ALPINE_VERSION} -n1 \
+        sh -c "docker run leonyork/pypi-releases ansible \
+        | tail -n $number_of_ansible_releases \
+        | xargs -I{ANSIBLE_VERSION} -n1 sh build-image.sh {ALPINE_VERSION} {ANSIBLE_VERSION} {ANSIBLE_VERSION}-alpine-{ALPINE_VERSION} || exit 255" || exit 255
+
+# Creates tags of the form {ANSIBLE_VERSION}-alpine (e.g. 2.9.2-alpine)
+# Gets the last $number_of_ansible_releases and creates an image using the latest alpine image
+docker run leonyork/pypi-releases ansible \
+        | tail -n $number_of_ansible_releases \
+        | xargs -I{ANSIBLE_VERSION} -n1 sh build-image.sh latest {ANSIBLE_VERSION} {ANSIBLE_VERSION}-alpine || exit 255
+
+# Generates the latest tag
+ansible_latest_version=`docker run leonyork/pypi-releases ansible | tail -n 1`
+sh build-image.sh latest $ansible_latest_version latest
